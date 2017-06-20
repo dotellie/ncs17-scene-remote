267c4eb0ee37fec07bf805438e6b9ec07f7a264e
Ellie <ellie@magnon.net>
2017-06-21 01:26:03 +0200

    Add style

--- src/front-end/main.js
+++ src/front-end/main.js
@@ -1,5 +1,7 @@
 import anime from "animejs";
 
+import "style-loader!./style.css";
+
 anime({
     targets: "#playpause-button polygon",
     points: "0 0 32 16 32 16 0 32",
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "out/"),
         filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" }
+        ]
     }
 };
