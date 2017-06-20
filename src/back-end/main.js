1d7c7938ccb3c280829bcd3c3160c6be5ddbce9f
DunderBoy <heden.johan@gmail.com>
2017-06-21 00:49:57 +0200

    Added api routes

--- src/back-end/main.js
+++ src/back-end/main.js
@@ -13,9 +13,42 @@ const port = "53000";
 
 const express = require("express");
 const osc = require("node-osc");
-const app = express.app();
-const client = new osc.client(serverIp, port);
+const app = express();
+const client = new osc.Client(serverIp, port);
 
+// Endroutes
+app.get("/", (req, res) => {
+
+});
+
+// ###############
+//  API Endroutes
+// ###############
+
+// Play Next
+app.get("/api/next", (req, res) => {
+    res.send("Sending next osc call to Qlab");
+    client.send("/GO");
+});
+
+// Playhead Previous:
+app.get("/api/previous", (req, res) => {
+
+});
+
+// Pause
+app.get("/api/pause", (req, res) => {
+
+});
+
+// Resume
+app.get("/api/resume", (req, res) => {
+
+});
+
+// ===========
+
+// Start listening for requests.
 app.listen(3000, () => {
     console.log("App is running");
 });
