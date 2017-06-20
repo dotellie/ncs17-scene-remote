7bacca3ad73b93741f4265de5ffc05475a49e677
Ellie <ellie@magnon.net>
2017-06-21 00:51:50 +0200

    Add template generator

---
+++ src/back-end/template-generator.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+
+const template = fs.readFileSync(path.resolve(__dirname, "./template.html"), "utf8");
+
+module.exports = apiUrl => {
+    return template.replace(/{{ ?apiUrl ?}})/g, apiUrl);
+};
