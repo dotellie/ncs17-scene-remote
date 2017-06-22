a1e64398b4b13f5e304637793a1cc0e8ce99cb3f
DunderBoy <heden.johan@gmail.com>
2017-06-22 17:24:54 +0200

    Refactor more code, Add osc promise factory.

--- src/back-end/main.js
+++ src/back-end/main.js
@@ -1,5 +1,6 @@
 const serverIp = "10.0.0.168";
 const port = "53000";
+const qlabWorkspaceId = "42C4DDC1-7E11-4215-BF4F-72E3DB17EC97";
 
 const express = require("express");
 const osc = require("node-osc");
@@ -97,15 +98,25 @@ app.post("/api/resume", (req, res) => {
 
 // ===========
 
-// Recive answers from Qlab.
-const server = new osc.Server(53001, "0.0.0.0");
-server.on("message", (msg, rinfo) => {
-    console.log(msg);
-});
-
 // Start listening for requests.
 app.listen(3000, () => {
     // console.log("App is running on: http://localhost:3000");
 });
 
-menu.show();
+// menu.show();
+
+const qlabSelectedQues = (workspaceId) => {
+    client.send(`/workspace/${workspaceId}/runningCues`);
+};
+
+const osc1 = require("./osc-sugar.js");
+osc1(`/workspace/${qlabWorkspaceId}/runningCues`).then(res => {
+    const cues = res.data.find(t => t.type === "Cue List" && t.armed).cues;
+    // console.log(cues.map(t => parseInt(t.number)));
+    const currentNumber = cues.map(t => parseInt(t.number)).reduce((a, t) => (a = Math.max(t, a)));
+    // const current = cues.find(t => t.number === `${currentNumber}`);
+    // console.log(current);
+    return osc1(`/cue/${currentNumber}/duration`);
+}).then(res => {
+    console.log(res);
+});
